b05d9d6277c27fb5e414ae28054516a02a201a78
isaaclopez <exolivil@gmail.com>
2022-06-13 12:10:20 -0500

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Jose Isaac on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
