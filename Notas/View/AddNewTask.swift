24bdc3d43ef59f3445143b6dc1a32a3f4e42684e
isaaclopez <exolivil@gmail.com>
2022-06-13 16:14:27 -0500

    add NewTask

---
+++ Notas/View/AddNewTask.swift
@@ -0,0 +1,20 @@
+//
+//  AddNewTask.swift
+//  Notas
+//
+//  Created by Jose Isaac on 13/06/22.
+//
+
+import SwiftUI
+
+struct AddNewTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewTask()
+    }
+}
